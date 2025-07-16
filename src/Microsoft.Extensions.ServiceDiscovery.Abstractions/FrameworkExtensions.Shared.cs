0acd73dec4dafc6896498a6ceebfbd0d0d25da98
Taylor Southwick <tasou@microsoft.com>
2025-07-16 12:54:37 -0700

    WIP: dns

--- src/Microsoft.Extensions.ServiceDiscovery.Abstractions/FrameworkExtensions.Shared.cs
+++ src/Microsoft.Extensions.ServiceDiscovery.Abstractions/FrameworkExtensions.Shared.cs
@@ -2,18 +2,35 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #if NETFRAMEWORK
+using System.Buffers;
 using System.Collections.Concurrent;
 using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
+using System.Text;
 
 namespace Microsoft.Extensions.ServiceDiscovery
 {
     internal static partial class FrameworkExtensions
     {
+        extension(StringSplitOptions)
+        {
+            public static StringSplitOptions TrimEntries => (StringSplitOptions)2;
+        }
+
         extension(string s)
         {
             public bool StartsWith(char c) => s is [{ } first, ..] && first == c;
+        }
+
+        extension(char)
+        {
+            public static bool IsAsciiLetterOrDigit(char c) => char.IsAsciiLetter(c) | char.IsBetween(c, '0', '9');
+            public static bool IsAsciiLetter(char c) => (uint)((c | 0x20) - 'a') <= 'z' - 'a';
+            public static bool IsBetween(char c, char minInclusive, char maxInclusive) =>
+                (uint)(c - minInclusive) <= (uint)(maxInclusive - minInclusive);
         }
 
         extension(ArgumentException)
@@ -47,6 +64,69 @@ namespace Microsoft.Extensions.ServiceDiscovery
             }
         }
 
+        extension(ValueTask)
+        {
+            public static ValueTask<T> FromResult<T>(T value)
+            {
+                return new ValueTask<T>(value);
+            }
+        }
+
+        extension(Span<byte> span)
+        {
+            public bool Contains(byte b)
+            {
+                return span.IndexOf(b) != -1;
+            }
+        }
+
+        extension(Encoding encoding)
+        {
+            public bool TryGetBytes(string str, Span<byte> bytes, out int bytesWritten)
+            {
+                int required = encoding.GetByteCount(str);
+                if (required <= bytes.Length)
+                {
+                    var result = encoding.GetBytes(str);
+
+                    if (result.Length <= bytes.Length)
+                    {
+                        result.CopyTo(bytes);
+                        bytesWritten = result.Length;
+                        return true;
+                    }
+                }
+
+                bytesWritten = 0;
+                return false;
+            }
+        }
+
+        extension(ArgumentOutOfRangeException)
+        {
+            public static void ThrowIfGreaterThan<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+                where T : IComparable<T>
+            {
+                if (value.CompareTo(other) > 0)
+                {
+                    ArgumentOutOfRangeException.Throw(value, other, paramName);
+                }
+            }
+
+            public static void ThrowIfLessThanOrEqual<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+                where T : IComparable<T>
+            {
+                if (value.CompareTo(other) <= 0)
+                {
+                    ArgumentOutOfRangeException.Throw(value, other, paramName);
+                }
+            }
+
+            [DoesNotReturn]
+            private static void Throw<T>(T value, T other, string? paramName) =>
+                throw new ArgumentOutOfRangeException(paramName, value, "Argument was out of range");
+        }
+
         extension(ObjectDisposedException)
         {
             public static void ThrowIf([DoesNotReturnIf(true)] bool condition, object instance)
@@ -64,6 +144,124 @@ namespace Microsoft.Extensions.ServiceDiscovery
             {
                 key = pair.Key;
                 value = pair.Value;
+            }
+        }
+
+        extension<TKey, TValue>(Dictionary<TKey, TValue> dictionary)
+        {
+            public bool TryAdd(TKey key, TValue value)
+            {
+                if (dictionary.ContainsKey(key))
+                {
+                    return false;
+                }
+                dictionary.Add(key, value);
+                return true;
+            }
+        }
+
+        extension<T>(ArraySegment<T>)
+        {
+            public static ArraySegment<T> Empty => new([], 0, 0);
+        }
+
+        extension(Socket socket)
+        {
+            public async Task<int> SendAsync(ReadOnlyMemory<byte> buffer, SocketFlags socketFlags, EndPoint endpoint, CancellationToken token)
+            {
+                token.ThrowIfCancellationRequested();
+
+                var rented = ArrayPool<byte>.Shared.Rent(buffer.Length);
+
+                try
+                {
+                    buffer.CopyTo(rented);
+                    var tcs = new TaskCompletionSource<int>();
+
+                    using var _ = token.Register(() => tcs.TrySetCanceled());
+
+                    using var args = new SocketAsyncEventArgs()
+                    {
+                        RemoteEndPoint = endpoint,
+                        SocketFlags = socketFlags,
+                    };
+
+                    args.SetBuffer(rented, 0, buffer.Length);
+
+                    args.Completed += (object sender, SocketAsyncEventArgs e) =>
+                    {
+                        if (e.ConnectByNameError is { } exception)
+                        {
+                            tcs.SetException(exception);
+                        }
+
+                        tcs.SetResult(e.BytesTransferred);
+                    };
+
+                    if (!socket.SendAsync(args))
+                    {
+                        // If it's false, the operation completed synchronously.
+                        tcs.SetResult(args.BytesTransferred);
+                    }
+
+                    return await tcs.Task.ConfigureAwait(false);
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(rented);
+                }
+            }
+
+            public async Task<int> ReceiveAsync(Memory<byte> buffer, SocketFlags socketFlags, CancellationToken token)
+            {
+                token.ThrowIfCancellationRequested();
+
+                var rented = ArrayPool<byte>.Shared.Rent(buffer.Length);
+
+                try
+                {
+                    var tcs = new TaskCompletionSource<int>();
+
+                    using var _ = token.Register(() => tcs.TrySetCanceled());
+
+                    using var args = new SocketAsyncEventArgs()
+                    {
+                        SocketFlags = socketFlags,
+                    };
+
+                    args.SetBuffer(rented, 0, buffer.Length);
+
+                    args.Completed += (object sender, SocketAsyncEventArgs e) =>
+                    {
+                        if (e.ConnectByNameError is { } exception)
+                        {
+                            tcs.SetException(exception);
+                        }
+
+                        tcs.SetResult(e.BytesTransferred);
+                    };
+
+                    if (!socket.ReceiveAsync(args))
+                    {
+                        // If it's false, the operation completed synchronously.
+                        tcs.SetResult(args.BytesTransferred);
+                    }
+
+                    var count = await tcs.Task.ConfigureAwait(false);
+
+                    rented.AsMemory(0, count).CopyTo(buffer);
+
+                    return count;
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(rented);
+                }
+            }
+
+            private void Args_Completed(object sender, SocketAsyncEventArgs e)
+            {
+                throw new NotImplementedException();
             }
         }
 
@@ -131,4 +329,32 @@ namespace System.Runtime.CompilerServices
         public string ParameterName => parameterName;
     }
 }
+
+namespace System.Diagnostics
+{
+    internal sealed class UnreachableException : Exception
+    {
+    }
+}
+
+namespace System.Buffers
+{
+    internal static class SearchValues
+    {
+        public static SearchValues<T> Create<T>(string str)
+        {
+            return new SearchValues<T>(str);
+        }
+    }
+
+    internal class SearchValues<T>
+    {
+        internal SearchValues(string str)
+        {
+            _ = str;
+            // Implementation for SearchValues
+        }
+    }
+}
+
 #endif
--- src/Microsoft.Extensions.ServiceDiscovery.Dns/Resolver/ResolvConf.cs
+++ src/Microsoft.Extensions.ServiceDiscovery.Dns/Resolver/ResolvConf.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Net;
@@ -8,12 +8,14 @@ namespace Microsoft.Extensions.ServiceDiscovery.Dns.Resolver;
 
 internal static class ResolvConf
 {
+#if NET
     [SupportedOSPlatform("linux")]
     [SupportedOSPlatform("osx")]
     public static ResolverOptions GetOptions()
     {
         return GetOptions(new StreamReader("/etc/resolv.conf"));
     }
+#endif
 
     public static ResolverOptions GetOptions(TextReader reader)
     {
